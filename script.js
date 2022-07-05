9747dcf2a7a3e793899f50daf39254cfd7542b55
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-07-05 04:01:56 +0300

    refactoring

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ function countdown() {
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const mins = Math.floor(totalSeconds / 60) % 60;
+    
     const seconds = Math.floor(totalSeconds) % 60;
 
+
     daysEl.innerHTML = days;
+
     hoursEl.innerHTML = formatTime(hours);
+
     minsEl.innerHTML = formatTime(mins);
+
     secondsEl.innerHTML = formatTime(seconds);
 }
 function formatTime(time) {
     return time < 10 ? `0${time}`: time;
 }
+
 
 countdown();
 setInterval(countdown, 1000);
